daab62f8b914bc1f7abe88a2114576620f75f449
njokojunior <njokojunior1@gmail.com>
2025-04-14 21:04:21 +0200

    Error Importing Images

---
+++ app.js
@@ -0,0 +1,5 @@
+import renderAuth from './src/view/authentication.js';
+
+const containerEl = document.querySelector('.container');
+
+containerEl.innerHTML = renderAuth;
---
+++ dist/Jaan Restaurant.0679e1a6.js
@@ -0,0 +1,2 @@
+console.log("hello");
+//# sourceMappingURL=Jaan Restaurant.0679e1a6.js.map
